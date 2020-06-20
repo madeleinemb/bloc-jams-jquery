7339e84ee47af0e659d7bd3cc275d45f646cd333
Maddie <madeleineut@gmail.com>
2020-06-20 00:21:53 -0500

    Assignment 17 Completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -18,7 +18,16 @@
 
       const nextSong = album.songs[nextSongIndex];
       player.playPause(nextSong);
+  });
 
+  $('#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const previousSongIndex = currentSongIndex-1;
+    if (previousSongIndex < 0) { return; }
+
+      const previousSong = album.songs[previousSongIndex];
+      player.playPause(previousSong);
   });
 
 
