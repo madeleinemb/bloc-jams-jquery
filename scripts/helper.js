1931c2eba3b59d51921f7fe2296a4c0106f97c6e
Maddie <madeleineut@gmail.com>
2020-06-21 15:37:22 -0500

    Removed conflicting code

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,17 +1,10 @@
 class Helper {
 
   playPauseAndUpdate(song) {
-<<<<<<< HEAD
-    player.playPause();
-    $('#time-control .total-time').text( song.duration );
-  }
-
-=======
     player.playPause(song);
     const totalTime = player.getDuration();
     $('#time-control .total-time').text( player.prettyTime(totalTime) );
   }
->>>>>>> Assignment-18
 }
 
 const helper = new Helper();
